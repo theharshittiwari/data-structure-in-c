e280025e5086847c0ed904e069eadd257446a6ef
theharshittiwari <145567459+theharshittiwari@users.noreply.github.com>
2023-11-07 14:42:48 +0530

    insertion of a node in the beginning of linked list.c

---
+++ insertion  of  a node in the beginning of linked list.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{ int choice=1;
+struct node{
+int data;
+struct node* next;
+};
+struct node* head,*newnode,*temp,*new;
+head=0;
+while(choice){
+newnode=(struct node*)malloc(sizeof(struct node));
+printf("Enter the node data:");
+scanf("%d",&newnode->data);
+newnode->next=0;
+if(head==0){
+head=temp=newnode;
+}
+else{
+temp->next=newnode;
+temp=newnode;
+}
+printf("do you want to continue (0,1)");
+scanf("%d",&choice);
+}
+new=(struct node*)malloc(sizeof(struct node));
+printf("enter the new node data");
+scanf("%d",&new->data);
+new->next=head;
+head=new;
+
+temp=head;
+while(temp!=0){
+printf("%d\t",temp->data);
+temp=temp->next;
+}
+
+
+    return 0;
+}
