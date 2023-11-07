8be40aa9f76a23e69e58d552c28ce1c797938f18
theharshittiwari <145567459+theharshittiwari@users.noreply.github.com>
2023-11-07 13:56:05 +0530

    implementation of liked list.c

---
+++ implementation of liked list.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{ int choice=1;
+struct node{
+int data;
+struct node* next;
+};
+struct node* head,*newnode,*temp;
+head=0;
+while(choice){
+newnode=(struct node*)malloc(sizeof(struct node));
+printf("Enter the node data:");
+scanf("%d",&newnode->data);
+newnode->next=0;
+if(head==0){
+head=temp=newnode;
+}
+else{
+temp->next=newnode;
+temp=newnode;
+}
+printf("do you want to continue (0,1)");
+scanf("%d",&choice);
+}
+temp=head;
+while(temp!=0){
+printf("%d\t",temp->data);
+temp=temp->next;
+}
+
+
+    return 0;
+}
