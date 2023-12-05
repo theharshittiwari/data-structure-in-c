503ac4d3de2effe4cc2ce3efca5041e6615d3785
theharshittiwari <145567459+theharshittiwari@users.noreply.github.com>
2023-12-06 00:00:15 +0530

    insertion of node at any position through function.c

---
+++ insertion of node at any position through function.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+int data;
+struct node* next;
+};
+int count=0;
+struct node* memoryallocation(){
+struct node* newnode;   
+newnode=(struct node*)malloc(sizeof(struct node));
+count=count+1;
+printf("enter the value of data in the newnode");
+scanf("%d",&newnode->data);
+newnode->next=NULL;
+return newnode;
+}
+struct node* create(struct node* head){
+struct node* newnode; 
+struct node* temp;
+newnode=memoryallocation();
+if(head==NULL){
+head=temp=newnode; 
+}
+else {
+temp=head;
+while(temp->next!=0){
+temp=temp->next;
+}
+temp->next=newnode;
+}
+return head;
+} 
+void insert(struct node* head){
+struct node* new,*temp;
+int i=1,pos;
+printf("enter the position");
+scanf("%d",&pos);
+if(pos>count){
+printf("the position is invalid");
+}
+else{
+temp=head;
+ while(i<pos){
+ temp=temp->next;    
+ i++;
+ } 
+ new=memoryallocation();
+ new->next=temp->next;
+ temp->next=new;
+}
+}
+void display(struct node* head){
+struct node* temp;
+temp=head;
+while(temp!=NULL){
+printf("%d\t\n",temp->data);
+temp=temp->next;
+}
+}
+void linkedlist(){
+struct node*head=NULL;
+int leave=1,choice;
+while(leave){
+printf("enter the choice(0,1,2,3)");
+scanf("%d",&choice);
+if(choice==0){
+leave=0;
+}
+if(choice==1){
+head=create(head);
+}
+if(choice==2){
+display(head);
+}
+if(choice==3){
+insert(head);
+}
+}
+}
+
+int main()
+{ linkedlist();
+ return 0;
+}
