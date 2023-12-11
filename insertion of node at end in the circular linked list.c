de62b69f34c236288057bcdf2cf5c70da99a33ac
theharshittiwari <145567459+theharshittiwari@users.noreply.github.com>
2023-12-12 00:57:00 +0530

    insertion of node at end in the circular linked list.c

---
+++ insertion of node at end in the circular linked list.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include<stdlib.h>
+struct node{
+int data;
+struct node* next;
+};
+struct node* tail;
+void create()
+{ int choice=1;
+tail=0;
+struct node* newnode;
+while(choice){
+newnode=(struct node*)malloc(sizeof(struct node));
+printf("Enter the node data:");
+scanf("%d",&newnode->data);
+newnode->next=0;
+if(tail==0){
+tail=newnode;
+tail->next=newnode;
+}
+else{
+newnode->next=tail->next;
+tail->next=newnode;
+tail=newnode;
+}
+printf("do you want to continue (0,1,2)");
+scanf("%d",&choice);
+}
+}
+void insert(){
+struct node* temp,*newnode;
+int choice=1;
+while(choice){
+newnode=(struct node*)malloc(sizeof(struct node));
+printf("Enter the node data:");
+scanf("%d",&newnode->data);
+newnode->next=0;
+temp=tail->next;
+tail->next=newnode;
+newnode->next=temp;
+tail=newnode;
+ printf("enter the choice(o or 1)");
+ scanf("%d",&choice);
+}
+}
+void display(){
+if(tail==0){
+printf("the list is empty");
+}
+else{
+struct node* temp;
+temp=tail->next;
+while(temp->next!=tail->next){
+printf("%d\t\n",temp->data);
+temp=temp->next;
+}
+printf("%d",temp->data);
+}
+}
+int main(){
+create();
+insert();
+display();
+return 0;
+}
