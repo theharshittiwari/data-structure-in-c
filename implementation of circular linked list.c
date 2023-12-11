5596e2b7438a6cb4a2c7ba5fdfc2d480bec6f6e5
theharshittiwari <145567459+theharshittiwari@users.noreply.github.com>
2023-12-11 23:25:27 +0530

    implementation of circular linked list.c

---
+++ implementation of circular linked list.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include<stdlib.h>
+struct node{
+int data;
+struct node* next;
+};
+struct node* tail;
+void create()
+{ int choice=1;
+tail=0;
+struct node* newnode;
+while(choice){
+newnode=(struct node*)malloc(sizeof(struct node));
+printf("Enter the node data:");
+scanf("%d",&newnode->data);
+newnode->next=0;
+if(tail==0){
+tail=newnode;
+tail->next=newnode;
+}
+else{
+newnode->next=tail->next;
+tail->next=newnode;
+tail=newnode;
+}
+printf("do you want to continue (0,1)");
+scanf("%d",&choice);
+}
+}
+void display(){
+if(tail==0){
+printf("the list is empty");
+}
+else{
+struct node* temp;
+temp=tail->next;
+while(temp->next!=tail->next){
+printf("%d\t\n",temp->data);
+temp=temp->next;
+}
+printf("%d",temp->data);
+}
+}
+int main(){
+create();
+display();
+return 0;
+}
