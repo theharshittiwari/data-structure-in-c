51bdc2a3c95bce10aec7086fd51efed9240a36df
theharshittiwari <145567459+theharshittiwari@users.noreply.github.com>
2023-12-19 18:30:37 +0530

    implementation of queue using linked list.c

---
+++ implementation of queue using linked list.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+int data;
+struct node* next;
+};
+struct node* front=0;
+struct node* rear=0;
+void enqueue(){
+struct node* newnode;
+newnode=(struct node*)malloc(sizeof(struct node));
+int x;
+printf("enter the data of newnode");
+scanf("%d",&x);
+newnode->data=x;
+newnode->next=0;
+if(front==0&&rear==0){
+front=rear=newnode;
+}
+else{
+rear->next=newnode;
+rear=newnode;
+}
+}
+
+void dequeue(){
+struct node* temp;
+if(front==0&&rear==0){
+printf("the position is invalid");
+}
+else{
+temp=front;
+printf("%d\t\n",temp->data);
+front=front->next;
+free(temp);
+}
+}
+
+void display(){
+struct node* temp;
+temp=front;
+while(temp!=0){
+printf("%d\t\n",temp->data);
+temp=temp->next;
+}
+}
+int main()
+{int n,choice=1;
+while(choice){
+printf("enter the value of n");
+scanf("%d",&n);
+switch(n){
+case 1:enqueue();
+break;
+case 2:dequeue();
+break;
+case 3:display();
+break;
+}
+}
+
+    return 0;
+}
